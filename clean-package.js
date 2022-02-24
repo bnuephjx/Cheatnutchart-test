3d793967cc89dac613777a87fbeaeb9ab11c40a7
evan <454002679@qq.com>
2022-02-24 10:21:31 +0800

    perf(clean-package.js): 重写

--- clean-package.js
+++ clean-package.js
@@ -1,22 +1,10 @@
 // https://stackoverflow.com/questions/40942495/clean-package-json-before-packing
-const fs = require('fs');
+const fs = require('fs-extra');
+const path = require('path');
 
-// create temp directory
-const tempDirectory = fs.mkdirSync(`clean`, {
-  recursive: true
-});
+const pkg = fs.readJSONSync(path.resolve(__dirname, 'package.json'));
 
-const packageDirectory = `${tempDirectory}/package`;
+delete pkg.scripts;
+delete pkg.devDependencies;
 
-// create subfolder package
-fs.mkdirSync(packageDirectory);
-
-// read existing package.json
-const packageJSON = require('./package.json');
-
-// create your own package.json or modify it here
-Reflect.deleteProperty(packageJSON, 'scripts');
-Reflect.deleteProperty(packageJSON, 'devDependencies');
-Reflect.deleteProperty(packageJSON, 'config');
-
-fs.writeFileSync(`${packageDirectory}/package.json`, JSON.stringify(packageJSON, null, 2));
+fs.writeFileSync(path.resolve(__dirname, 'package.json'), JSON.stringify(pkg, null, 2));
